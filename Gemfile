7b8a50372aa93d4c464732c2474247609049f614
Joshua Toper <josh@borrowmydoggy.com>
2017-09-02 15:57:40 +0100

    Dockerise!

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source 'https://rubygems.org'
 
 ruby '2.4.0'
 
+gem 'exchange_rate_jt'
 gem 'sinatra'
 gem 'sinatra-contrib'
-gem 'exchange_rate_jt'
--- app.rb
+++ app.rb
@@ -3,5 +3,8 @@ require 'sinatra/reloader'
 require 'exchange_rate_jt'
 require_relative 'config/config'
 
+set :bind, '0.0.0.0'
+
 get '/' do
+  "We are go!"
 end
